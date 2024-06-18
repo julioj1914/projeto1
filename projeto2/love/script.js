bcc89125c2818fcccfe9517c3f46c4b9e41934e2
julioj1914 <julinhosantosrodrigues@gmail.com>
2024-06-18 19:49:30 -0300

    comit

--- projeto2/love/script.js
+++ projeto2/love/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const arrow = document.querySelector('.arrow');
+    const arrow = document.getElementById('arrow');
     const message = document.querySelector('.message');
 
     arrow.addEventListener('animationend', () => {
