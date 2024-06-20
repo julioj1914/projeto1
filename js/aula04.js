936882595f85f16bce64db54f6de3c9353d1a7cb
julioj1914 <julinhosantosrodrigues@gmail.com>
2024-06-20 13:00:04 -0300

    usando if

---
+++ js/aula04.js
@@ -0,0 +1,8 @@
+var vel = 70
+console.log (`a velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 80) {
+    console.log ('você ultrapassou a velocidade permitida!!!')
+} 
+
+console.log ('dirija sempre usando o sinto de segurança')
