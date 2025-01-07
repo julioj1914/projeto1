97e6f1945d37be795f64f9fc8e1606ed4e61874a
julioj1914 <julinhosantosrodrigues@gmail.com>
2025-01-07 17:42:02 -0300

    d

--- js/operadorthis.js
+++
@@ -1,9 +0,0 @@
-function aluno(nome,nota) {
-    this.nome=nome
-    this.nota=nota
-
-    console.log(nome)
-    console.log(nota)
-}
-
-aluno('julio',9)
--- js/variavelcomposta.js
+++
@@ -1,5 +0,0 @@
-num = [2,1,4,5,9,6]
-
-for (let pos in num) {
-    console.log (`a posição ${pos} tem o valor ${num[pos]}`)
-}
